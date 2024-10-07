01e7bc61d83fa95fce6acff8bd29fcc932ad1bf6
Ikendu <ndubest56@gmail.com>
2024-10-07 11:03:46 +0100

    more files

---
+++ index.js
@@ -0,0 +1,16 @@
+function ShowTime() {
+  document.getElementById("date").innerHTML = Date();
+  document.getElementById("date").style.color = "blue";
+  document.getElementById("date").style.fontSize = "50px";
+  //   console.log("called");
+}
+
+document.getElementById("remove").innerHTML = "The text was changed";
+// alert("Hello World");
+function submit(e) {
+  e;
+  // sending to database
+  // saving in database
+  // display to the user
+  print();
+}
