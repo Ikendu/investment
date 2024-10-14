715209b47a3e68fd6eade1f6ce4983146c92db31
Ikendu <ndubest56@gmail.com>
2024-10-14 12:09:13 +0100

    more on javascript

---
+++ index.js
@@ -0,0 +1,47 @@
+myName = "Peter Obi";
+let name;
+let student = "";
+var num = 10;
+const car = "Ikenga";
+
+// car = "Movati";
+const alpha = "D";
+let cars = ["Merc", "Toyota", "Ranger", "Corrola", 100, 31];
+
+console.log(num, "First");
+
+num = num * 100;
+
+console.log(num, "Second");
+
+alert(car);
+// alert(ans);
+
+num = num + 500;
+
+addValue(1000, 500);
+
+console.log(num);
+
+console.log(car);
+document.getElementById("display").innerText = "Show my visitors";
+
+function doSomething() {
+  let car = "Igwe";
+  console.log(car);
+}
+
+function addValue(a, b) {
+  let added = a + b;
+console.log(added);
+  return added;
+}
+
+addValue(10, 20);
+addValue(11, 200);
+addValue(30, 20);
+
+let result = addValue(100, 80);
+
+let finalResult = result + 200;
+console.log(finalResult);
