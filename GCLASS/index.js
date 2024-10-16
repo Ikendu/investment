7cdbceefe728195ee5d122000c59c126d04c84c6
Ikendu <ndubest56@gmail.com>
2024-10-16 12:13:57 +0100

    into javascript

--- GCLASS/index.js
+++ GCLASS/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+var intro = "Hello World I";
+
+console.log(intro);
+
+alert(intro);
+
+document.write(intro);
+
+intro = 1000;
+
+document.getElementById("second").innerHTML = intro;
+
+let myName = "David XP";
+myName = "Gift Onyinye";
+
+const myLikes = "Be very wealthy and healthy";
+
+console.log(myLikes);
