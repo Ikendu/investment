f6aa83a07ec034710217f926b9e97b5f3b9273a3
Ikendu <ndubest56@gmail.com>
2024-10-28 11:06:19 +0100

    more on scripting

---
+++ ECLASS/index.js
@@ -0,0 +1,29 @@
+let initial = "A";
+let student;
+
+let myName = "David Obi";
+
+let names = ["David", "Gift", "Mich", "Peter"];
+let group = { first: "David", second: "Gift", third: "Mich", fourth: "Peter" };
+
+let age = 18;
+let age2 = 20;
+
+let ans = myName + age + 10;
+let ans2 = age + 10 + myName;
+
+console.log(ans);
+console.log(ans2);
+console.log(age > age2);
+
+if (age < age2) {
+  let result = 20 + 30;
+  console.log(result + " is gotten because our result is true");
+}
+console.log(names[3]);
+console.log(group.second);
+console.log(group.third);
+console.log(group.fourth);
+console.log(student);
+
+console.log("Final");
