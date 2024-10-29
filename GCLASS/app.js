08c347bc7eebcf86330582209054bfd15299194b
Ikendu <ndubest56@gmail.com>
2024-10-29 14:54:43 +0100

    more on javascript

--- GCLASS/app.js
+++ GCLASS/app.js
@@ -1,2 +1,26 @@
+// let Philip = 'A student of Digital" Dreams';
 let myName = "Philip";
+
 console.log(myName);
+
+let num1 = 4;
+let num2 = 20;
+let num3 = 10;
+let num4 = 2;
+
+let fullName = `${myName} Ogbu is ${num2} years old`;
+
+let hisName = myName + " Ogbu is " + num2 + " years old";
+
+console.log(fullName);
+console.log(hisName);
+num1++;
+num1 = num1 + 2;
+num1 += 2;
+
+let text1 = 386;
+let text2 = 4;
+
+let result = text1 == text2;
+
+console.log(result);
--- GCLASS/index.js
+++ GCLASS/index.js
@@ -1,18 +1,48 @@
 var intro = "Hello World I";
 
-console.log(intro);
+// console.log(intro);
 
-alert(intro);
+// alert(intro);
 
-document.write(intro);
+// document.write(intro);
 
 intro = 1000;
 
-document.getElementById("second").innerHTML = intro;
+// document.getElementById("second").innerHTML = intro;
 
 let myName = "David XP";
 myName = "Gift Onyinye";
 
 const myLikes = "Be very wealthy and healthy";
 
-console.log(myLikes);
+let ans = 5;
+ans++;
+ans++;
+ans += 5;
+
+// console.log(ans);
+
+let result = 10;
+result = result + 70;
+result = result + 10;
+
+result = result + 100;
+result++;
+result = result + 10.2;
+// console.log(result);
+// console.log(div);
+
+function printer() {
+  let firstName = "Davis ";
+  let secondName = "Obinna ";
+  let surName = "Eneh";
+  let add = 10 + 30;
+
+  let id = firstName + secondName + surName + " is a man of integrity";
+  let idss = "David " + "Obi" + " Ene";
+  console.log(id);
+  console.log(idss);
+  console.log(add);
+}
+let moreR = `here is the result before ${printer()}`;
+console.log(moreR);
