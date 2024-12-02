41dcd695f5770d6f29851609fed26df2ace0fd15
Ikendu <ndubest56@gmail.com>
2024-12-02 14:54:25 +0100

    new script class

--- GCLASS2/index.js
+++ GCLASS2/index.js
@@ -1,6 +1,27 @@
-var myName;
-var myName = "David";
+const myName = "David";
+let num = 10 % 3;
 
-for (let i = 0; i < myName.length; i++) {
-  console(i);
-}
+// myName = "Helen";
+
+// myName = "Ikenna";
+console.log(myName);
+console.log(num);
+
+num = 100;
+num = num ** 2;
+// num++;
+// num *= 2;
+
+console.log(num);
+
+console.log(9 !== 9);
+
+let newName;
+console.log(newName);
+
+let name1 = "50";
+let name2 = 20;
+let name3 = 10;
+
+let allName = name1 + name2 + name3;
+console.log(allName);
